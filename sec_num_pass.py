a6b33959dbb85fca74fc1c221abd95ed3469e173
Danis <askhatov.danis89@yandex.ru>
2025-06-30 16:41:06 +0300

    30.06.25

--- sec_num_pass.py
+++ sec_num_pass.py
@@ -6,22 +6,20 @@ uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 punctuation = "'!#$%&*+-=?@^_."
 num_pass = 0  # Подсчет кол-ва паролей для форматир. вывода.
 
-def int_num(b):  # Фун-я для перевода вх.параметра в int если параметр число.
+def int_num(b):  # Фун-я для перевода вх.параметра в int если параметр число, число должно быть больше 0.
     usr = input(b)
-    return int(usr) if usr.isdigit() else print("Введите цифру для ответа")  # Вх. параметр число или нет.
+    return int(usr) if usr.isdigit() and int(usr) > 0 else print("Введите цифру для ответа, больше 0")  # Вх. параметр число или нет.
 
 def yes_no(c):  # Фун-я для проверки ответа пользователя. Ввел пользователь да или нет
     while True:
         usr = input(c).lower()
-
         if usr == 'да' or usr == 'нет':
             return usr
         else:
             print('Для точного ответа введите да или нет ')
 
-def par_true(a):
-    usr = a
-    return usr if usr == 'да' else None
+def par_true(usr): # Фун-я для проверки положительного ответа пользователя
+    return usr if usr != 'нет' else None
 
 while True:
     pass_quan_usr = int_num("Введите кол-во паролей которое нужно сгенерировать ... ")
@@ -33,24 +31,38 @@ while True:
     if len_pass:
         break
 
-dig_yes_or_no = yes_no("Включать ли цифры 0123456789. Введите да/нет ... ")
-low_let_yes_or_no = yes_no("Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz. Введите да/нет ... ")
-upper_let_yes_or_no = yes_no("Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ. Введите да/нет ... ")
-pun_let_yes_or_no = yes_no("Включать ли символы '!#$%&*+-=?@^_.. Введите да/нет ... ")
-amb_chr_yes_or_no = yes_no("Исключать ли неоднозначные символы il1Lo0O. Введите да/нет ... ")
+def sry(j):
+    flag = 
+    for i in j:
+        if i != 'да':
+            print('Один из ответов должен быть положительным')
 
-lst = [dig_yes_or_no, low_let_yes_or_no, upper_let_yes_or_no, pun_let_yes_or_no]
+while True:
 
-if par_true(amb_chr_yes_or_no):
-    digits = digits.replace('0','')
+    dig_yes_or_no = yes_no("Включать ли цифры 0123456789. Введите да/нет ... ")
+    low_let_yes_or_no = yes_no("Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz. Введите да/нет ... ")
+    upper_let_yes_or_no = yes_no("Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ. Введите да/нет ... ")
+    pun_let_yes_or_no = yes_no("Включать ли символы '!#$%&*+-=?@^_.. Введите да/нет ... ")
+    amb_chr_yes_or_no = yes_no("Исключать ли неоднозначные символы il1Lo0O. Введите да/нет ... ")
 
-print(digits)
+    lst = [dig_yes_or_no, low_let_yes_or_no, upper_let_yes_or_no, pun_let_yes_or_no]
 
+
+# else:
+#     break
+
+
+# if par_true(amb_chr_yes_or_no):
+#     digits = digits[2:-1]
+#     lowercase_letters = lowercase_letters.translate(str.maketrans('', '', 'ijo'))
+#     uppercase_letters = uppercase_letters.translate(str.maketrans('', '', 'LO'))
+#     punctuation = punctuation[:14]
+#
 # for i in range(pass_quan_usr):
 #     cnt_len_pass = len_pass
 #     chars = []
 #
-#     while cnt_len_pass >= 0:
+#     while True:
 #
 #         if par_true(lst[0]) and cnt_len_pass > 0:
 #             chars.append(choice(digits))
@@ -64,9 +76,9 @@ print(digits)
 #         if par_true(lst[3]) and cnt_len_pass > 0:
 #             chars.append(choice(punctuation))
 #             cnt_len_pass -= 1
-#         else:
+#         elif cnt_len_pass == 0:
 #             break
 #     num_pass += 1
 #
 #     shuffle(chars)
-#     print(f'Пароль №{num_pass} ', *chars, sep='')
+#     print(f'Пароль №{num_pass}: ', *chars, sep='')
